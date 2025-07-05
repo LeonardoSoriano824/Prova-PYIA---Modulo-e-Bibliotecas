5fc363a9edee9213809ad58157b2e180e3b42b2e
Leo <leonardo.sobrinho864@gmail.com>
2025-07-05 17:45:02 -0300

    Removendo comentario

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# [PYIA-A07] Crie uma função chamada lancar_dados que utilizará o módulo random para simular o lançamento de dois dados. 
-# Cada dado deve gerar um número aleatório entre 1 e 6. 
-# A função deve somar os resultados desses dois lançamentos e retornar o valor total.
-
 import random
 
 def lancar_dados():
